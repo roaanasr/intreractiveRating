98f98c74e3b16325dd795bc794b0e25492f4e758
Roaa <roaanasr96@gmail.com>
2025-08-28 16:58:56 +0300

    interactiveRating

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const container=document.querySelector(".container")
+const submitbtn=document.getElementById("Submit-btn")
+const thankyou=document.getElementById("thankyou")
+const ratingBtns=document.querySelectorAll(".rating")
+let selected=document.querySelector(".dynamic-number")
+let defaultScore=0;
+submitbtn.addEventListener("click",submitScore);
+ratingBtns.forEach((btn)=>{
+    btn.addEventListener("click",ratingBtnsClicked)
+})
+
+function submitScore(e){
+    e.preventDefault();
+    container.style.display="none";
+    thankyou.style.display="block";
+    selected.textContent=defaultScore;
+}
+function ratingBtnsClicked(e){
+    if(e.target.classList.contains("rating")){
+        e.target.classList.toggle("active");
+    }
+    defaultScore=e.target.textContent;
+        console.log("الرقم المختار هو:", defaultScore);
+        console.log("العنصر اللي هيظهر فيه الرقم:", selected);
+}
