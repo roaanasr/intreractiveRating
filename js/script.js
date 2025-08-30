71a3030b6ac3e43385865621dd9ef8fa33c0713e
Roaa <roaanasr96@gmail.com>
2025-08-30 15:36:02 +0300

    updating

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@ const submitbtn=document.getElementById("Submit-btn")
 const thankyou=document.getElementById("thankyou")
 const ratingBtns=document.querySelectorAll(".rating")
 let selected=document.querySelector(".dynamic-number")
-let defaultScore=0;
+let defaultScore=null;
 submitbtn.addEventListener("click",submitScore);
 ratingBtns.forEach((btn)=>{
     btn.addEventListener("click",ratingBtnsClicked)
-})
+});
 
 function submitScore(e){
     e.preventDefault();
+    if(defaultScore===null){
+        alert("please select a rating before submitting!")
+        return;
+    }
     container.style.display="none";
     thankyou.style.display="block";
-    selected.textContent=defaultScore;
+    // selected.textContent= ""+defaultScore+out5;
+    selected.textContent=`${defaultScore}out of 5`
 }
 function ratingBtnsClicked(e){
-    if(e.target.classList.contains("rating")){
-        e.target.classList.toggle("active");
-    }
-    defaultScore=e.target.textContent;
-        console.log("الرقم المختار هو:", defaultScore);
-        console.log("العنصر اللي هيظهر فيه الرقم:", selected);
-}
+    e.preventDefault();
+    ratingBtns.forEach((btn)=>btn.classList.remove("avtive"))
+    e.target.classList.add("active")
+    defaultScore=e.target.textContent;}
